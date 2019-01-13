8858b6456aac760d03915eb0909a1e3fd7eb20be
Deepankar Agrawal <deepankar11093@gmail.com>
2019-01-14 03:26:22 +0530

    add katex for math rendering

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 ---
 ---
 window.onload = function(){
-  document.getElementById("menu-icon").onclick=function(){
+  document.getElementById("menu-icon").onclick=function() {
     toggleClass(document.body, "exp");
   }
+  inflateKatex();
 }
 
 // util functions
-function toggleClass(element, toggleClass){
+function toggleClass(element, toggleClass) {
   var currentClass = element.className;
   var newClass;
   if(currentClass.split(" ").indexOf(toggleClass) > -1){ //has class
@@ -16,4 +17,13 @@ function toggleClass(element, toggleClass){
      newClass = currentClass + " " + toggleClass;
   }
   element.className = newClass.trim();
+}
+
+function inflateKatex() {
+  var classes = document.getElementsByClassName("equation");
+  for (var i = 0; i < classes.length; i++) {
+    classes[i].innerHTML = katex.renderToString(classes[i].innerHTML.trim(), {
+      throwOnError: false
+    });
+  }
 }
