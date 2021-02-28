cd8b61944f02267e2ba7e2f69dd4b6c0857d730f
Deepankar Agrawal <deepankar11093@gmail.com>
2021-03-01 04:03:38 +0530

    add dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,35 @@
+setTheme();
+
 window.onload = function () {
   document.getElementById("menu-icon").onclick = function () {
     toggleClass(document.body, "exp");
   }
+  document.getElementById("dark-theme-icon").onclick = function () {
+    toggleTheme();
+  }
   inflateKatex();
+}
+
+function setTheme() {
+  if (localStorage.getItem("theme") === "dark") {
+    document.getElementsByTagName("body")[0].setAttribute("theme", "dark");
+    document.getElementById("dark-theme-icon").setAttribute("title", "Switch to light theme");
+  }
+}
+
+function toggleTheme() {
+  var currentTheme = localStorage.getItem("theme");
+  if (currentTheme == null || currentTheme == "light") {
+    localStorage.setItem("theme", "dark");
+    document.getElementsByTagName("body")[0].setAttribute("theme", "dark");
+
+    document.getElementById("dark-theme-icon").setAttribute("title", "Switch to light theme");
+  } else {
+    localStorage.setItem("theme", "light");
+    document.getElementsByTagName("body")[0].removeAttribute("theme");
+
+    document.getElementById("dark-theme-icon").setAttribute("title", "Switch to dark theme");
+  }
 }
 
 // util functions
@@ -27,3 +54,26 @@ function inflateKatex() {
     });
   }
 }
+
+// create a basic game loop controller to run simulations at target FPS
+window.raf = (function() {
+  return window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function(callback) {window.setTimeout(callback, 1000 / 60);};
+})();
+
+var GameLoopController = {
+  loop: function(loopFunc, targetFps) {
+      window.raf(function(now){
+          var dt = now - GameLoopController.stamp || 0;
+          GameLoopController.stamp = now;
+          loopFunc(dt);
+          GameLoopController.loop(loopFunc, targetFps);
+      });
+
+  },
+  stamp: undefined
+};
