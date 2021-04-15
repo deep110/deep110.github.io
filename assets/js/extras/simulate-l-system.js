e5e925632e6c7483710a1caa3ca86edc58fe5e7e
Deepankar Agrawal <deepankar11093@gmail.com>
2021-04-15 19:19:47 +0530

    update lsystems blog

--- assets/js/extras/simulate-l-system.js
+++ assets/js/extras/simulate-l-system.js
@@ -1,80 +1,128 @@
 let canvas;
 let context;
-var currentSystem;
-var sentence;
+let lsystem;
+
+var labels = {
+  "context_free": "Context Free Deterministic Grammar",
+  "context_sensitive": "Context Sensitive Grammar",
+  "stochastic": "Stochastic Grammar"
+}
 
 var systems = {
-  "tree": {
-    "axiom": "F",
-    "axiomAngle": 25,
-    "lengthMul": 0.5,
-    "startPositionY": 0.5,
-    "rules": [
-      {
-        "a": 'F',
-        "b": 'FF+[+F-F-F]-[-F+F+F]'
-      },
-    ]
+  "context_free": {
+    "system1": {
+      "axiom": "F",
+      "axiomAngle": 25,
+      "startLength": 200,
+      "lengthMul": 0.5,
+      "startPositionY": 0.5,
+      "type": 0,
+      "rules": [
+        {
+          "a": 'F',
+          "b": 'FF+[+F-F-F]-[-F+F+F]'
+        },
+      ]
+    },
+    "system2": {
+      "axiom": "X",
+      "axiomAngle": 25,
+      "startLength": 200,
+      "lengthMul": 0.5,
+      "startPositionY": 0.5,
+      "type": 0,
+      "rules": [
+        {
+          "a": 'F',
+          "b": 'FF'
+        },
+        {
+          "a": 'X',
+          "b": 'F+[-F-XF-X][+FF][--XF[+X]][++F-X]'
+        },
+      ]
+    },
+    "system3": {
+      "axiom": "X",
+      "axiomAngle": 25,
+      "startLength": 200,
+      "lengthMul": 0.53,
+      "startPositionY": 0.3,
+      "type": 0,
+      "rules": [
+        {
+          "a": 'F',
+          "b": 'FX[FX[+XF]]'
+        },
+        {
+          "a": 'X',
+          "b": 'FF[+XZ++X-F[+ZX]][-X++F-X]'
+        },
+        {
+          "a": 'Z',
+          "b": '[+F-X-F][++ZX]'
+        }
+      ]
+    },
+    "system4": {
+      "axiom": "X",
+      "axiomAngle": 22,
+      "startLength": 200,
+      "lengthMul": 0.53,
+      "startPositionY": 0.4,
+      "type": 0,
+      "rules": [
+        {
+          "a": 'X',
+          "b": 'F-[[X]+X]+F[+FX]-X'
+        },
+        {
+          "a": 'F',
+          "b": 'FF'
+        }
+      ]
+    }
   },
-  "tree1": {
-    "axiom": "X",
-    "axiomAngle": 25,
-    "lengthMul": 0.5,
-    "startPositionY": 0.5,
-    "rules": [
-      {
-        "a": 'F',
-        "b": 'FF'
-      },
-      {
-        "a": 'X',
-        "b": 'F+[-F-XF-X][+FF][--XF[+X]][++F-X]'
-      },
-    ]
+  "context_sensitive": {
+    "system1": {
+      "axiom": "FYFYFY",
+      "axiomAngle": 30,
+      "startLength": 100,
+      "lengthMul": 0.85,
+      "startPositionY": 0.5,
+      "ignore": "\\+|-|F",
+      "type": 1,
+      "rules": [
+        {
+          "a": 'F < F',
+          "b": 'F'
+        }
+      ]
+    }
   },
-  "tree2": {
-    "axiom": "X",
-    "axiomAngle": 25,
-    "lengthMul": 0.55,
-    "startPositionY": 0.3,
-    "rules": [
-      {
-        "a": 'F',
-        "b": 'FX[FX[+XF]]'
-      },
-      {
-        "a": 'X',
-        "b": 'FF[+XZ++X-F[+ZX]][-X++F-X]'
-      },
-      {
-        "a": 'Z',
-        "b": '[+F-X-F][++ZX]'
-      }
-    ]
-  },
-  "tree3": {
-    "axiom": "X",
-    "axiomAngle": 33,
-    "lengthMul": 0.5,
-    "startPositionY": 0.5,
-    "rules": [
-      {
-        "a": 'XF',
-        "b": 'XXF'
-      },
-      {
-        "a": 'XX',
-        "b": 'XXX'
-      }
-    ]
+  "stochastic": {
+    "system1": {
+      "axiom": "F",
+      "axiomAngle": 25,
+      "startLength": 200,
+      "lengthMul": 0.5,
+      "startPositionY": 0.5,
+      "type": 2,
+      "rules": [
+        {
+          "a": 'F',
+          "b": 'FF+[+F-F-F]-[-F+F+F]'
+        },
+      ]
+    }
   }
 }
 
 class Turtle {
-  constructor() {
-    this.axiomAngle = 0;
-    this.length = 100;
-    this.startPosY = 0;
+  constructor(axiomAngle, startPosY, startLength) {
+    this.axiomAngle = axiomAngle;
+    this.length = startLength;
+    this.startPosY = startPosY;
     this.position = new Vector2(0, 0);
     this.stack = [];
     this.resetPosition();
@@ -93,7 +141,7 @@ class Turtle {
     } else if (char == '-') {
       this.angle += this.axiomAngle;
     } else if (char == '[') {
-      this.stack.push({"positionX": this.position.x, "positionY": this.position.y, "angle": this.angle});
+      this.stack.push({ "positionX": this.position.x, "positionY": this.position.y, "angle": this.angle });
     } else if (char == ']') {
       let val = this.stack.pop();
       this.position.set(val.positionX, val.positionY);
@@ -111,76 +159,182 @@ class Turtle {
     this.stack = [];
   }
 
-  reset(axiomAngle, posY) {
+  reset(axiomAngle, posY, startLength) {
     this.resetPosition();
     this.axiomAngle = axiomAngle * 0.01745;
     this.startPosY = posY;
-    this.length = 100;
+    this.length = startLength;
   }
 }
 
-function generate() {
-  var rules = currentSystem.rules;
-  turtle.length *= currentSystem.lengthMul;
-  var nextSentence = '';
-  for (var i = 0; i < sentence.length; i++) {
-    var current = sentence.charAt(i);
-    var found = false;
-    for (var j = 0; j < rules.length; j++) {
-      if (current == rules[j].a) {
-        found = true;
-        nextSentence += rules[j].b;
-        break;
+class LSystem2 {
+  constructor(axiom, type, rules, ignoredSymbols) {
+    this.axiom = axiom;
+    this.type = type;
+    this.rules = rules;
+    this.ignoredSymbols = ignoredSymbols;
+    this.ignoreSymbolRegex = new RegExp(ignoredSymbols, "g");
+    this.code = axiom;
+  }
+
+  iterate() {
+    console.log("curr code: ", this.code);
+    var nextSentence = '';
+    for (var i = 0; i < this.code.length; i++) {
+      var current = this.code.charAt(i);
+
+      switch (this.type) {
+        case 0:
+          nextSentence += this.matchForContextFree(current, this.rules);
+          break;
+        case 1:
+          nextSentence += this.matchForContextSensitive(i, this.code, this.rules, this.ignoreSymbolRegex);
+          break;
       }
     }
-    if (!found) {
-      nextSentence += current;
-    }
+
+    this.code = nextSentence;
+    // this.code = lsystemT.iterate();
+
+    console.log( "next code: ", this.code)
+    // console.log( "truth    : ", lsystemT.iterate())
+    console.log("--------------------------")
+    return this.code;
   }
-  sentence = nextSentence;
-  render();
+
+  getString() {
+    return this.code;
+  }
+
+  matchForContextFree(codeChar, rules) {
+    for (var j = 0; j < rules.length; j++) {
+      if (codeChar == rules[j].a) {
+        return rules[j].b;
+      }
+    }
+    return codeChar;
+  }
+  
+  matchForContextSensitive(index, code, rules, ignoreStringReg) {
+    var pre = code.substring(0, index).replace(ignoreStringReg, '')
+    var post = code.substring(index+1, code.length).replace(ignoreStringReg, '')
+  
+    // console.log( "pre: ", pre)
+    // console.log( "post: ", post)
+  
+    return code[index]
+  }
+
+  reset() {
+    this.code = this.axiom;
+  }
 }
 
-function reset() {
-  sentence = currentSystem.axiom;
-  turtle.reset(currentSystem.axiomAngle, currentSystem.startPositionY);
-  render();
-}
-
-function render() {
+function render(code) {
   turtle.resetPosition();
+  turtle.length *= currentSystem.lengthMul;
   context.clearRect(0, 0, canvas.width, canvas.height);
 
   context.beginPath();
-  for (var i = 0; i < sentence.length; i++) {
-    turtle.draw(context, sentence.charAt(i));
+  for (var i = 0; i < code.length; i++) {
+    turtle.draw(context, code.charAt(i));
   }
   context.strokeStyle = "#FFFFFF80";
   context.stroke();
+}
+
+function setupInteractiveSystem() {
+  let select = document.getElementById("system-selector");
+  for (const sys_name in systems) {
+    var optGrp = document.createElement("optgroup");
+    optGrp.label = labels[sys_name]
+    optGrp.id = sys_name
+    for (const ex in systems[sys_name]) {
+      var option = document.createElement("option");
+      option.value = ex;
+      option.text = ex;
+      optGrp.appendChild(option);
+    }
+    select.appendChild(optGrp);
+  }
+  fillDescription(currentSystem);
+
+  select.addEventListener("change", () => {
+	  var optgroupId = select.options[select.selectedIndex].parentNode.id;
+    currentSystem = systems[optgroupId][select.value]
+
+    fillDescription(currentSystem);
+    lsystem = new LSystem2(
+      currentSystem.axiom,
+      currentSystem.type,
+      currentSystem.rules,
+      currentSystem.ignore,
+    );
+    turtle.reset(currentSystem.axiomAngle, currentSystem.startPositionY, currentSystem.startLength);
+    render(lsystem.getString());
+
+  }, false);
+}
+
+function fillDescription(cs) {
+  document.getElementById("desc-axiom").innerHTML = "Axiom: " + cs["axiom"]
+  document.getElementById("desc-angle").innerHTML = "Angle: " + cs["axiomAngle"]
+
+  var ruleStr = "Rules: "
+  var rules = cs["rules"]
+  for (let i=0; i < rules.length; i++) {
+    ruleStr += "<br>" + rules[i]["a"] + " -> " + rules[i]["b"]
+  }
+
+  document.getElementById("desc-rules").innerHTML = ruleStr
 }
 
 window.addEventListener('DOMContentLoaded', (_) => {
   // setup
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
+  currentSystem = systems["context_sensitive"]["system1"];
+
+  turtle = new Turtle(currentSystem.axiomAngle, currentSystem.startPositionY, currentSystem.startLength);
+  lsystem = new LSystem2(
+    currentSystem.axiom,
+    currentSystem.type,
+    currentSystem.rules,
+    currentSystem.ignore,
+  );
 
   setupInteractiveSystem();
-  turtle = new Turtle();
-  reset();
+
+  lsystemT = new LSystem({
+    axiom: "FYFYFY",
+    productions: {
+      "X<X>X": "Y",
+      "X<X>Y": "Y[-FYFY]",
+      "X<Y>X": "Y",
+      "X<Y>Y": "Y",
+      "Y<X>X": "X",
+      "Y<X>Y": "YFY",
+      "Y<Y>X": "X",
+      "Y<Y>Y": "X",
+      "+": "-",
+      "-": "+",
+    },
+    "ignoredSymbols": "+-F&^/|\\"
+  })
+
+  // lsystemT.iterate(4)
+
+  document.getElementById("generate").addEventListener("click", function() {
+    // generate next iteration
+    var code = lsystem.iterate();
+
+    // render the next generated iteration
+    render(code);
+  });
+
+  document.getElementById("reset").addEventListener("click", function() {
+    turtle.reset(currentSystem.axiomAngle, currentSystem.startPositionY, currentSystem.startLength);
+    lsystem.reset();
+    render(lsystem.getString());
+  });
 });
-
-function setupInteractiveSystem() {
-  let select = document.getElementById("system-selector");
-  for (const sys in systems) {
-    var option = document.createElement("option");
-    option.value = sys;
-    option.text = sys;
-    select.appendChild(option);
-  }
-  currentSystem = systems[select.value];
-
-  select.addEventListener("change", ()=> {
-    currentSystem = systems[select.value];
-    reset();
-  }, false);
-}
--- posts/blogs/2021-03-07-natural-trees-with-stochastic-L-system.md
+++ posts/blogs/2021-03-07-natural-trees-with-stochastic-L-system.md
@@ -2,12 +2,11 @@
 layout: blog.liquid
 title:  "Natural Looking Trees with Stochastic L-Systems"
 categories: ["blog"]
-is_draft: true
 data:
-  scripts: [extras/simulate-l-system.js]
+  scripts: [extras/simulate-l-system.js, extras/lindenmayer.js]
 ---
 
-[No Man's Sky](https://www.nomanssky.com/) is built around a procedurally generated deterministic open world universe with too many planets to be explored realistically. It is claimed that they used [Superformula](https://en.wikipedia.org/wiki/Superformula) and [L-Systems](https://en.wikipedia.org/wiki/L-system) for generating flora and  fauna procedurally. I tried experimenting with L systems to create realistic looking trees and described my results here.
+[No Man's Sky](https://www.nomanssky.com/) is built around a procedurally generated deterministic open world universe with too many planets to be explored realistically. It is claimed that they used [Superformula](https://en.wikipedia.org/wiki/Superformula) and [L-Systems](https://en.wikipedia.org/wiki/L-system) for generating flora and  fauna procedurally. So, I tried playing around with L systems and see if I can create some realistic looking trees.
 
 ### What is a L-System
 An L-system or [Lindenmayer system](https://en.wikipedia.org/wiki/L-system) is a recursive and string-rewriting framework that was discovered by Hungarian biologist Aristid Lindenmayer while studying algae growth. Nowadays, in computer graphics it is used for creating procedural and fractal like structures. I won't go into much depth about it, since it is described quite elaborately on wikipedia.
@@ -17,7 +16,7 @@ There are mainly three types of L systems:
     - Context Free
     - Context Sensitive
 2. Stochastic Grammar
-3. Parametric Grammar
+3. Parametric Grammar [Not covered in this blog]
 
 <style>
   #container {
@@ -27,7 +26,7 @@ There are mainly three types of L systems:
   canvas {
     background: #3a3a3a;
     display: block;
-    margin: 0 10px 10px 0;
+    margin: 0 20px 10px 0;
   }
   #dropdown-container {
     display: inline-block;
@@ -38,7 +37,7 @@ There are mainly three types of L systems:
       flex-direction: column;
     }
     #canvas {
-        width: calc(100vw - 30px);
+        width: calc(100vw - 2em);
     }
   }
 </style>
@@ -47,18 +46,18 @@ There are mainly three types of L systems:
   <div id="canvas-container">
     <canvas id="canvas" height=400 width=500></canvas>
     <div id="controls-container">
-        <button id="generate" onclick="generate()">Generate</button>
-        <button id="reset" onclick="reset()">Reset</button>
+        <button id="generate">Generate</button>
+        <button id="reset">Reset</button>
         <div id="dropdown-container">
-          <label>Choose: </label>
+          <label>Choose the system: </label>
           <select id="system-selector"></select>
         </div>
     </div>
   </div>
   <div id="lsystem-description">
-    <p>Axiom: FF</p>
-    <p>System Description</p>
-    <p>Rules: F-> FF</p>
+    <p id="desc-axiom">Axiom: FF</p>
+    <p id="desc-angle">Angle: 35</p>
+    <p id="desc-rules">Rules: F-> FF</p>
   </div>
 </div>
 
@@ -94,8 +93,8 @@ Consider the following system:
 ```js
 Axiom = BAAAAA
 Rules => [
-  BA -> BB
-  B  -> A
+  B < A -> B
+  B -> A
 ]
 ```
 
@@ -109,18 +108,45 @@ Pass4 => AAAABA
 Pass5 => AAAAAB
 ```
 
+In the above example, sometimes people get confused that if first `B` turns into to `A` then how does second A turns to B. It is because you have two generations of axiom, you don't change the second letter based on new value of first letter but old value of first letter. For example in `Pass0 -> Pass1` if I go letter by letter and apply rules:
+```js
+// pass0 -> pass1
+     B   ->   A  // rule 2
+     A   ->   B  // rule 1 because before A there is a B
+     A   ->   A  // no rule match hence same
+     A   ->   A  // no rule match hence same
+     A   ->   A  // no rule match hence same
+     A   ->   A  // no rule match hence same
+```
+
+
 ### Stochastic Grammar
 
-You cannot generate infinite variations of even same type of tree with these deterministic methods. So, for creating an infinite world we can take probabilistic approach to provide more randomness to the generated fractal trees.
+You cannot generate infinite variations of even same type of tree with these deterministic methods. So, for creating an infinite world we need to introduce some sort of randomness to the generated axioms.
+
+
+
+
 
 
 <h3 id="rendering">Rendering</h3>
 
 Hello world
 
+
+### Footnotes
+[^fn1]:. Here X is just a dummy variable for rendering i.e it will get ignored. For more in-depth see [rendering](#rendering) section. Dummy variables are used to give a more realistic look to generated fractal trees.
+
+<br>
+
 ### References
 - [The Algorithmic Beauty of Plants](http://algorithmicbotany.org/papers/#abop)
 - [Job Talle - Lindenmayer Systems](https://jobtalle.com/lindenmayer_systems.html)
 
-### Footnotes
-[^fn1]:. Here X is just a dummy variable for rendering i.e it will get ignored. For more indepth see rendering section. Dummy variables are used to give a more realistic look to the generate fractal trees.
+
+### TODO
+
+- add stochastic section
+- add contextual code to js
+- add rendering section with good images
+- add images to deterministic one
